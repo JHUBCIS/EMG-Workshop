5b0135b4e4d41eada312615bc90d35a436f966db
Brian <106613884+briansebzhou@users.noreply.github.com>
2023-09-27 16:06:11 -0400

    replace movingAvg w/ RunningAverage

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <EMA_Filters.h>
-#include <movingAVG.h>
+#include <RunningAverage.h>
 #include <NoDelay.h>
 #include <Servo.h>
 
@@ -10,7 +10,8 @@ Servo svo; // servo motor
 /*create instances for data processing*/
 EMA_Filters emaFilt; 
 float f_s = 400; // sampling frequency
-movingAvg mvavg(10); //average latest 10 data points
+const int avg_window = 10;
+RunningAverage rnAvg(avg_window);
 
 /*EMG parameters*/
 int emg_pin = 0; // analog input pin
@@ -33,24 +34,23 @@ void setup() {
 
   svo.attach(svo_pin); 
   svo.write(svo_pos);
-  mvavg.begin();
 
   /*resting EMG signal collection for calibration*/ 
   Serial.println("Resting EMG collection for calibration");
   float emg_stat_sum = 0;
   for (int i=0; i<10; i++)
   {
-      for (int j=0; j<100; j++)
-      {
-          emg_stat_sum += analogRead(emg_pin);
-          delay(10);
-      }
-      // led.setLevel(1 + i); 
-      Serial.print("Done in ");
-      Serial.println(10-i);
+    Serial.print("Done in ");
+    Serial.println(10-i);
+    for (int j=0; j<100; j++)
+    {
+        emg_stat_sum += analogRead(emg_pin);
+        delay(10);
+    } 
   }
-  Serial.println("Done!");
   emg_stat = emg_stat_sum / 1000.0;
+  Serial.print("Done! Resting EMG is: ");
+  Serial.println(emg_stat);
 }
 
 
@@ -63,15 +63,17 @@ void loop() {
   float f_c_bphigh = 150; // high cut off frequency
   float emg_bp = emaFilt.BPF(emg_raw, f_c_bplow, f_c_bphigh, f_s); // bandpassed emg signal between 50 Hz and 150 Hz
   // plot signals using Teleplot
-  // Serial.print(">raw EMG signal: ");
-  // Serial.println(emg_raw);
+  Serial.print(">raw EMG signal: ");
+  Serial.println(emg_raw);
   Serial.print(">bandpass EMG signal: ");
   Serial.println(emg_bp);
 
   /*capture EMG singal envelope*/
   float f_c_lp = 1; // lowpass cut off frequency 
   float emg_evlp =  pow(emaFilt.LPF(abs(emg_bp), f_c_lp, f_s), 2); // take magnitude, lowpass, then square
-  int emg_evlp_avg = mvavg.reading(emg_evlp); // further smoothing
+  rnAvg.addValue(emg_evlp); // update the buffers
+  float emg_evlp_avg = rnAvg.getAverage();
+  // plot signals using Teleplot
   Serial.print(">bandpass EMG signal envelope: ");
   Serial.println(emg_evlp_avg);
   
